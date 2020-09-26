aa7ec584f38bae3c451015a13e83eee2aa35bb38
Ronaldi Chandra <ronhyun@protonmail.com>
2020-09-26 20:02:45 +0700

    MASIH BUG DI MILIH CONTINUE GAN

--- t_akhir.java
+++ t_akhir.java
@@ -2,11 +2,15 @@ import java.util.Scanner;
 
 public class t_akhir {
     public static void main(String[] args) {
+        menu();
+    }
 
-        int pilih;
+    public static void menu() {
+        int pilih = 0;
         Scanner inp = new Scanner(System.in);
+        do{
         clrscr();
-        System.out.println("KALKULATOR");
+        System.out.println("-[KALKULATOR]-\n");
         System.out.println("1. Penjumlahan");
         System.out.println("2. Pengurangan");
         System.out.println("3. Perkalian");
@@ -28,66 +32,108 @@ public class t_akhir {
                 pembagian();
                 break;
             case 5:
-                System.out.println("Exiting...");
+                exit();
                 break;
         }
-        inp.close();
+        }while(pilih<5);
     }
 
     public static void penjumlahan() {
         int angka1, angka2, hasil;
+        String pilih2 = "n";
         Scanner inp = new Scanner(System.in);
         clrscr();
+        System.out.println("-[PENJUMLAHAN]-\n");
         System.out.print("Masukkan angka pertama: ");
         angka1 = inp.nextInt();
         System.out.print("Masukkan angka kedua: ");
         angka2 = inp.nextInt();
         hasil = angka1 + angka2;
-        inp.close();
         clrscr();
+        System.out.println("-[PENJUMLAHAN]-\n");
         System.out.println("Hasilnya adalah: " + hasil);
+        System.out.print("Continue (y/n) ? ");
+        pilih2 = inp.next();
+        if(pilih2 == "y"){
+            menu();
+        } else if(pilih2 == "n"){
+            exit();
+        }
     }
 
     public static void pengurangan(){
         int angka1, angka2, hasil;
+        String pilih2;
         Scanner inp = new Scanner(System.in);
         clrscr();
+        System.out.println("-[PENGURANGAN]-\n");
         System.out.print("Masukkan angka pertama: ");
         angka1 = inp.nextInt();
         System.out.print("Masukkan angka kedua: ");
         angka2 = inp.nextInt();
         hasil = angka1 - angka2;
-        inp.close();
         clrscr();
+        System.out.println("-[PENGURANGAN]-\n");
         System.out.println("Hasilnya adalah: " + hasil);
+        System.out.print("Continue (y/n) ? ");
+        pilih2 = inp.next();
+        switch(pilih2){
+            case "y":
+                menu();
+                break;
+            case "n":
+                break;
+        }
     }
 
     public static void perkalian(){
         int angka1, angka2, hasil;
+        String pilih2;
         Scanner inp = new Scanner(System.in);
         clrscr();
+        System.out.println("-[PERKALIAN]-\n");
         System.out.print("Masukkan angka pertama: ");
         angka1 = inp.nextInt();
         System.out.print("Masukkan angka kedua: ");
         angka2 = inp.nextInt();
         hasil = angka1 * angka2;
-        inp.close();
         clrscr();
+        System.out.println("-[PERKALIAN]-\n");
         System.out.println("Hasilnya adalah: " + hasil);
+        System.out.print("Continue (y/n) ? ");
+        pilih2 = inp.next();
+        switch(pilih2){
+            case "y":
+                menu();
+                break;
+            case "n":
+                break;
+        }
     }
 
     public static void pembagian(){
         int angka1, angka2, hasil;
+        String pilih2;
         Scanner inp = new Scanner(System.in);
         clrscr();
+        System.out.println("-[PEMBAGIAN]-\n");
         System.out.print("Masukkan angka pertama: ");
         angka1 = inp.nextInt();
         System.out.print("Masukkan angka kedua: ");
         angka2 = inp.nextInt();
         hasil = angka1 / angka2;
-        inp.close();
         clrscr();
+        System.out.println("-[PEMBAGIAN]-\n");
         System.out.println("Hasilnya adalah: " + hasil);
+        System.out.print("Continue (y/n) ? ");
+        pilih2 = inp.next();
+        switch(pilih2){
+            case "y":
+                menu();
+                break;
+            case "n":
+                break;
+        }
     }
 
     public static void clrscr() {  
@@ -95,4 +141,8 @@ public class t_akhir {
         System.out.flush();  
     }
 
+    public static void exit() {
+        clrscr();
+        System.out.println("Exiting...");
+    }
 }
