eaf9d68d428f055d3233cb28f9dbb0e54340feeb
Ronaldi Chandra <ronhyun@protonmail.com>
2020-11-12 20:21:06 +0700

    Add files via upload

---
+++ tugasakhir.java
@@ -0,0 +1,25 @@
+package TugasAkhir;
+
+public class tugasakhir {
+    public static void main(String[] args) {
+        d_harga();
+    }
+
+    public static void d_harga(){
+        System.out.format("+-----------------------------------+ %n");
+        System.out.format("+            DAFTAR MENU            + %n");
+        System.out.format("+--------------------+-------------+ %n");
+        System.out.format("|   |      Menu      |    Harga    | %n");
+        System.out.format("+---+----------------+-------------+ %n");
+        System.out.format("| 1 |  Nasi Goreng   |  Rp. 13000  | %n");
+        System.out.format("| 2 |  Mie Ayam      |  Rp. 15000  | %n");
+        System.out.format("| 3 |  Nasi Bakar    |  Rp. 18000  | %n");
+        System.out.format("| 4 |  Sop Kambing   |  Rp. 25000  | %n");
+        System.out.format("+----------------------------------+ %n");
+        System.out.format("| 5 |  Jus Jambu     |  Rp. 7000   | %n");
+        System.out.format("| 6 |  Jus Jeruk     |  Rp. 5000   | %n");
+        System.out.format("| 7 |  Air Mineral   |  Rp. 3000   | %n");
+        System.out.format("| 8 |  Teh Manis     |  Rp. 2000   | %n");
+        System.out.format("+--------------------+-------------+ %n");
+    }
+}
