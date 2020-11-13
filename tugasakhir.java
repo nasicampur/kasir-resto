9d4f5b4b32790b571baf7b5a02ddf0a637f2b1e9
Ronaldi Chandra <ronhyun@protonmail.com>
2020-11-13 10:42:25 +0700

    Update tugasakhir.java

--- tugasakhir.java
+++ tugasakhir.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class tugasakhir {
+
+    static Scanner inp = new Scanner(System.in);
+    static int jumInput, pesanMenu, harga[];
     public static void main(String[] args) {
-        System.out.println("\n\n                                 APLIKASI KASIR \n");
         d_harga();
+        menu();
     }
 
     public static void d_harga(){
+        System.out.println("\n\n                                 APLIKASI KASIR \n");
         System.out.format("+------------------------------------+    +------------------------------------+ %n");
         System.out.format("+        DAFTAR MENU MAKANAN         +    +        DAFTAR MENU MINUMAN         + %n");
         System.out.format("+----------------------+-------------+    +----------------------+-------------+ %n");
@@ -16,4 +22,32 @@ public class tugasakhir {
         System.out.format("|  4  |  Sop Kambing   |  Rp. 25000  |    |  8  |  Teh Manis     |  Rp. 2000   | %n");
         System.out.format("+----------------------+-------------+    +----------------------+-------------+%n");
     }
+
+    public static void menu() {
+        harga = new int[99];
+        System.out.print("Berapa menu yang ingin diinput? ");
+        jumInput = inp.nextInt();
+        for (int i = 1; i <= jumInput; i++) {
+            System.out.print("Pilih menu yang dipesan: ");
+            pesanMenu = inp.nextInt();
+            switch (pesanMenu) {
+                case 1:
+                    harga[i] = 13000;
+                    break;
+                case 2:
+                    harga[i] = 15000;
+                    break;
+                case 3:
+                    harga[i] = 18000;
+                    break;
+                case 4:
+                    harga[i] = 25000;
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+    }
 }
+
