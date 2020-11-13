7f299e8142234ab4be9db5ec7378764ab0135f60
Ronaldi Chandra <ronhyun@protonmail.com>
2020-11-13 10:22:54 +0700

    crot

--- tugasakhir.java
+++ tugasakhir.java
@@ -1,23 +1,19 @@
 public class tugasakhir {
     public static void main(String[] args) {
+        System.out.println("\n\n                                 APLIKASI KASIR \n");
         d_harga();
     }
 
     public static void d_harga(){
-        System.out.format("+----------------------------------+ %n");
-        System.out.format("+           DAFTAR MENU            + %n");
-        System.out.format("+--------------------+-------------+ %n");
-        System.out.format("|   |      Menu      |    Harga    | %n");
-        System.out.format("+---+----------------+-------------+ %n");
-        System.out.format("| 1 |  Nasi Goreng   |  Rp. 13000  | %n");
-        System.out.format("| 2 |  Mie Ayam      |  Rp. 15000  | %n");
-        System.out.format("| 3 |  Nasi Bakar    |  Rp. 18000  | %n");
-        System.out.format("| 4 |  Sop Kambing   |  Rp. 25000  | %n");
-        System.out.format("+----------------------------------+ %n");
-        System.out.format("| 5 |  Jus Jambu     |  Rp. 7000   | %n");
-        System.out.format("| 6 |  Jus Jeruk     |  Rp. 5000   | %n");
-        System.out.format("| 7 |  Air Mineral   |  Rp. 3000   | %n");
-        System.out.format("| 8 |  Teh Manis     |  Rp. 2000   | %n");
-        System.out.format("+--------------------+-------------+ %n");
+        System.out.format("+------------------------------------+    +------------------------------------+ %n");
+        System.out.format("+        DAFTAR MENU MAKANAN         +    +        DAFTAR MENU MINUMAN         + %n");
+        System.out.format("+----------------------+-------------+    +----------------------+-------------+ %n");
+        System.out.format("| No. |      Menu      |    Harga    |    | No. |      Menu      |    Harga    | %n");
+        System.out.format("+-----+----------------+-------------+    +----------------------+-------------+ %n");
+        System.out.format("|  1  |  Nasi Goreng   |  Rp. 13000  |    |  5  |  Jus Jambu     |  Rp. 7000   | %n");
+        System.out.format("|  2  |  Mie Ayam      |  Rp. 15000  |    |  6  |  Jus Jeruk     |  Rp. 5000   | %n");
+        System.out.format("|  3  |  Nasi Bakar    |  Rp. 18000  |    |  7  |  Air Mineral   |  Rp. 3000   | %n");
+        System.out.format("|  4  |  Sop Kambing   |  Rp. 25000  |    |  8  |  Teh Manis     |  Rp. 2000   | %n");
+        System.out.format("+----------------------+-------------+    +----------------------+-------------+%n");
     }
 }
