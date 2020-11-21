f948ebe38a3c1644cc99edd60256abdc04c76a9c
Ronaldi Chandra <ronhyun@protonmail.com>
2020-11-21 12:37:27 +0700

    Bahan: - 500 gr daging kambing - 1 bawang bombay, iris - 5 bawang merah iris - 2 kayu manis - 5 cengkeh - 1/2 biji pala - 3 kapulaga, dihaluskan - 1 tomat, potong-potong - Gula - Garam  Bahan bumbu halus: - 8 Bawang putih - 1 jempol jahe - 2 cm kunyit - 1,5 sdt jinten bubuk - 1,5 sdt lada bubuk  Cara membuat: 1. Rebus daging kambing sampai setengah matang 2. Panaskan minyak, masukkan kayu manis, bawang merah dan bawang bombay, tumis hingga harum dan layu. Masukkan bumbu halus, pala, cengkeh. Tumis sampai bumbu benar-benar matang. Lalu masukkan tumisan bumbu ke rebusan daging 3. Masukkan kapulaga dan bumbui dengan garam dan gula, cek rasa. Masak sampai daging empuk 4. Setelah daging empuk dan cek rasa lagi, masukkan potongan tomat. Masak sampai bumbu meresap. Hidangkan dengan taburan bawang merah goreng.

--- tugasakhir.java
+++ tugasakhir.java
@@ -1,3 +1,4 @@
+package TugasAkhir;
 import java.util.Scanner;
 
 public class tugasakhir {
@@ -13,16 +14,16 @@ public class tugasakhir {
     
     public static void d_harga(){
         System.out.println("\n\n                                 APLIKASI KASIR \n");
-        System.out.format("+------------------------------------+    +------------------------------------+ %n");
-        System.out.format("+        DAFTAR MENU MAKANAN         +    +        DAFTAR MENU MINUMAN         + %n");
-        System.out.format("+-----+----------------+-------------+    +-----+----------------+-------------+ %n");
-        System.out.format("| No. |      Menu      |    Harga    |    | No. |      Menu      |    Harga    | %n");
-        System.out.format("+-----+----------------+-------------+    +-----+----------------+-------------+ %n");
-        System.out.format("|  1  |  Nasi Goreng   |  Rp. 13000  |    |  5  |  Jus Jambu     |  Rp. 7000   | %n");
-        System.out.format("|  2  |  Mie Ayam      |  Rp. 15000  |    |  6  |  Jus Jeruk     |  Rp. 5000   | %n");
-        System.out.format("|  3  |  Nasi Bakar    |  Rp. 18000  |    |  7  |  Air Mineral   |  Rp. 3000   | %n");
-        System.out.format("|  4  |  Sop Kambing   |  Rp. 25000  |    |  8  |  Teh Manis     |  Rp. 2000   | %n");
-        System.out.format("+-----+----------------+-------------+    +-----+----------------+-------------+ %n%n");
+        System.out.println("+------------------------------------+    +------------------------------------+");
+        System.out.println("+        DAFTAR MENU MAKANAN         +    +        DAFTAR MENU MINUMAN         +");
+        System.out.println("+-----+----------------+-------------+    +-----+----------------+-------------+");
+        System.out.println("| No. |      Menu      |    Harga    |    | No. |      Menu      |    Harga    |");
+        System.out.println("+-----+----------------+-------------+    +-----+----------------+-------------+");
+        System.out.println("|  1  |  Nasi Goreng   |  Rp. 13000  |    |  5  |  Jus Jambu     |  Rp. 7000   |");
+        System.out.println("|  2  |  Mie Ayam      |  Rp. 15000  |    |  6  |  Jus Jeruk     |  Rp. 5000   |");
+        System.out.println("|  3  |  Nasi Bakar    |  Rp. 18000  |    |  7  |  Air Mineral   |  Rp. 3000   |");
+        System.out.println("|  4  |  Sop Kambing   |  Rp. 25000  |    |  8  |  Teh Manis     |  Rp. 2000   |");
+        System.out.println("+-----+----------------+-------------+    +-----+----------------+-------------+\n");
     }
 
     public static void menu() {
